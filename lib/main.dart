0a112dfdc4026838a087a263ee5a2f3e3cd8196e
Mustafa Özdemir <mustafaozdemiroffical@gmail.com>
2023-11-12 20:06:09 +0300

    Expense App End

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:expenseapp/pages/expenses_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: ExpensesPage(),
+    ),
+  ));
+}
+// modelleme
+
